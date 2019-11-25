2676d5e3214e22bda99654c968a57a7bf7fcfdcf
Myeongjong <kmj.stat@gmail.com>
2019-11-24 23:54:09 -0600

    [Build] Initialize a basic test skeleton

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(GPsim)
+
+test_check("GPsim")
---
+++ tests/testthat/test-simulation.R
@@ -0,0 +1,13 @@
+####################################################################################
+###   Author: Myeongjong Kang (kmj.stat@gmail.com)
+###
+###   Overview: This script includes several tests for simulation functions.
+###
+###   Description:
+###
+###
+####################################################################################
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
