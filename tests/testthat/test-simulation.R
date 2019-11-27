0d2f5fc5aec0442884654988663360c49410a203
Myeongjong <kmj.stat@gmail.com>
2019-11-26 18:25:18 -0600

    [Fix] Reflect the changed function name in the test procedure

--- tests/testthat/test-simulation.R
+++ tests/testthat/test-simulation.R
@@ -9,18 +9,18 @@
 ####################################################################################
 
 test_that("locs, n, and p", {
-  expect_equal(.input_check_locs_n_p(locs = matrix(0, 3, 2), n = NULL, p = NULL),
-               .input_check_locs_n_p(locs = matrix(0, 3, 2), n = 3, p = 2))
-  expect_equal(.input_check_locs_n_p(locs = matrix(0, 3, 2), n = NULL, p = NULL),
-               .input_check_locs_n_p(locs = matrix(0, 3, 2), n = 3, p = NULL))
-  expect_equal(.input_check_locs_n_p(locs = matrix(0, 3, 2), n = NULL, p = NULL),
-               .input_check_locs_n_p(locs = matrix(0, 3, 2), n = NULL, p = 2))
-  expect_equal(as.matrix(.input_check_locs_n_p(locs = 'grid', n = 3^2, p = 2)$locs),
-               as.matrix(.input_check_locs_n_p(locs = expand.grid(c(0, 0.5, 1), c(0, 0.5, 1)), n = NULL, p = NULL)$locs))
-  expect_identical(tryCatch(.input_check_locs_n_p(locs = matrix(0, 3, 2), n = NULL, p = 1), error = function(e) TRUE), TRUE)
-  expect_identical(tryCatch(.input_check_locs_n_p(locs = matrix(0, 3, 2), n = 4, p = NULL), error = function(e) TRUE), TRUE)
-  expect_identical(tryCatch(.input_check_locs_n_p(locs = matrix(0, 3, 2), n = 3, p = 1), error = function(e) TRUE), TRUE)
-  expect_identical(tryCatch(.input_check_locs_n_p(locs = matrix(0, 3, 2), n = 4, p = 2), error = function(e) TRUE), TRUE)
+  expect_equal(.checkargs_locsnp(locs = matrix(0, 3, 2), n = NULL, p = NULL),
+               .checkargs_locsnp(locs = matrix(0, 3, 2), n = 3, p = 2))
+  expect_equal(.checkargs_locsnp(locs = matrix(0, 3, 2), n = NULL, p = NULL),
+               .checkargs_locsnp(locs = matrix(0, 3, 2), n = 3, p = NULL))
+  expect_equal(.checkargs_locsnp(locs = matrix(0, 3, 2), n = NULL, p = NULL),
+               .checkargs_locsnp(locs = matrix(0, 3, 2), n = NULL, p = 2))
+  expect_equal(as.matrix(.checkargs_locsnp(locs = 'grid', n = 3^2, p = 2)$locs),
+               as.matrix(.checkargs_locsnp(locs = expand.grid(c(0, 0.5, 1), c(0, 0.5, 1)), n = NULL, p = NULL)$locs))
+  expect_identical(tryCatch(.checkargs_locsnp(locs = matrix(0, 3, 2), n = NULL, p = 1), error = function(e) TRUE), TRUE)
+  expect_identical(tryCatch(.checkargs_locsnp(locs = matrix(0, 3, 2), n = 4, p = NULL), error = function(e) TRUE), TRUE)
+  expect_identical(tryCatch(.checkargs_locsnp(locs = matrix(0, 3, 2), n = 3, p = 1), error = function(e) TRUE), TRUE)
+  expect_identical(tryCatch(.checkargs_locsnp(locs = matrix(0, 3, 2), n = 4, p = 2), error = function(e) TRUE), TRUE)
 })
 
 test_that("availability of arguments", {
