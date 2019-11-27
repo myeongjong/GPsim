0e296543745fc1b3af0aafb05caf3a25bd6f3266
Myeongjong <kmj.stat@gmail.com>
2019-11-26 18:35:04 -0600

    [Fix] Simplify the test codes wrt the fact that setting a random seed is redundant

--- tests/testthat/test-simulation.R
+++ tests/testthat/test-simulation.R
@@ -41,7 +41,6 @@ test_that("meanmodel", {
 })
 
 test_that("availability of arguments", {
-  set.seed(2)
   expect_equal(simulate_gp_brute(locs = 'random', n = 3, p = 2, meanmodel = function(loc, meanparms) 0, meanparms = NULL, covmodel = function(loc1, loc2, covparms) ifelse(identical(loc1, loc2), 1, 0), covparms = NULL, seed = 1)$y,
                simulate_gp_brute(locs = 'random', n = 3, p = 2, meanmodel = function(loc, meanparms) 0, meanparms = NULL, covmodel = function(loc1, loc2, covparms) ifelse(identical(loc1, loc2), 1, 0), covparms = NULL, seed = 1)$y)
   expect_equal(simulate_gp_brute(locs = 'random', n = 3, p = 2, meanmodel = function(loc, meanparms) 0, meanparms = NULL, covmodel = function(loc1, loc2, covparms) ifelse(identical(loc1, loc2), 1, 0), covparms = NULL, seed = 1)$y,
